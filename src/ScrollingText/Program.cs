4b6f461b7d031996a79967a8f0f73fbb3ab32b7f
Eric Bach <bach.eric@gmail.com>
2021-12-19 13:36:38 -0700

    Test

--- src/ScrollingText/Program.cs
+++ src/ScrollingText/Program.cs
@@ -17,6 +17,38 @@ namespace ScrollingText
         public decimal Change { get; set; }
     }
 
+    public class Quote
+    {
+        public QuoteSummary quoteSummary { get; set; }
+
+        public class QuoteSummary
+        {
+            public ICollection<Result> result { get; set; }
+
+            public class Result
+            {
+                public Price price { get; set; }
+
+                public class Price
+                {
+                    public string symbol { get; set; }
+                    public RegularMarketPrice regularMarketPrice { get; set; }
+                    public RegularMarketChange regularMarketChange { get; set; }
+
+                    public class RegularMarketPrice
+                    {
+                        public decimal raw { get; set; }
+                    }
+
+                    public class RegularMarketChange
+                    {
+                        public decimal raw { get; set; }
+                    }
+                }
+            }
+        }
+    }
+
     public class Program
     {
         private static RGBLedCanvas _canvas;
